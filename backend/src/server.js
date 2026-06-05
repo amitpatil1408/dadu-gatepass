dd46fea25d54ef223e57f3bbe89017ae0f7d984d
Amit Patil <amit1408patil@gmail.com>
2026-06-05 20:18:44 +0530

    Setup PostgreSQL connection and core backend structure

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,26 @@
 const express = require("express");
+const pool = require("./config/db");
+require("dotenv").config();
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "DADU Gatepass API Running",
-  });
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.json({
+     message: "DADU Gatepass API Running",
+      database: "Connected",
+      time: result.rows[0].now,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Database connection failed",
+      error: error.message,
+    });
+  }
 });
 
 app.listen(PORT, () => {
